f82e764de1dda9bbc33b7a60ac9fadcc6d1d5fa5
Jake Kantzer <jkantzer@outlook.com>
2025-12-31 21:48:38 -0500

    Updated properties, tabs to spaces

--- Core/Apocalypse_Localization.sql
+++ Core/Apocalypse_Localization.sql
@@ -2,10 +2,10 @@
 -- APOCALYPSE MODE IMPROVED: LOCALIZATION
 --=============================================================================================================
 INSERT OR REPLACE INTO LocalizedText
-		(Language,	Tag,	Text)
-VALUES	('en_US',	'LOC_RANDOM_EVENT_COMET_STRIKE_DESCRIPTION',
-		'Comets pummel the planet.'),
-		('en_US',	'LOC_RANDOM_EVENT_COMET_STRIKE_EFFECT_DESCRIPTION',
-		'- Will destroy anything it lands on, including City Centers.'),
-		('en_US',	'LOC_PEDIA_CONCEPTS_PAGE_ENVIRONMENTAL_EFFECTS_CHAPTER_COMETS_PARA_1',
-		'Comet Strikes wipe out anything in their path, including City Centers.');
+        (Language,    Tag,    Text)
+VALUES  ('en_US',    'LOC_RANDOM_EVENT_COMET_STRIKE_DESCRIPTION',
+        'Comets pummel the planet.'),
+        ('en_US',    'LOC_RANDOM_EVENT_COMET_STRIKE_EFFECT_DESCRIPTION',
+        '- Will destroy anything it lands on, including City Centers.'),
+        ('en_US',    'LOC_PEDIA_CONCEPTS_PAGE_ENVIRONMENTAL_EFFECTS_CHAPTER_COMETS_PARA_1',
+        'Comet Strikes wipe out anything in their path, including City Centers.');
--- Core/Apocalypse_RandomEvents.sql
+++ Core/Apocalypse_RandomEvents.sql
@@ -1,8 +1,11 @@
 -- Allow random Comet Strikes and targeted Comet Strikes to happen at any time
 -- Allow all disasters to happen at the Apocalypse stage
+-- Increase odds of Comet Strikes at the Apocalypse stage
+-- Increase odds of Meteor Showers
+
 INSERT OR REPLACE INTO RandomEvent_Frequencies 
-		(RandomEventType,	RealismSettingType,				OccurrencesPerGame)
-SELECT	RandomEventType,	'REALISM_SETTING_APOCALYPSE',	OccurrencesPerGame
+        (RandomEventType,    RealismSettingType,                OccurrencesPerGame)
+SELECT   RandomEventType,    'REALISM_SETTING_APOCALYPSE',      OccurrencesPerGame
 FROM RandomEvent_Frequencies 
 WHERE RealismSettingType = 'REALISM_SETTING_MEGADISASTERS';
 
@@ -11,8 +14,10 @@ SET OccurrencesPerGame = OccurrencesPerGame*2
 WHERE RealismSettingType = 'REALISM_SETTING_APOCALYPSE' AND OccurrencesPerGame <> 1;
 
 INSERT OR REPLACE INTO RandomEvent_Frequencies
-		(RandomEventType,						RealismSettingType,					OccurrencesPerGame)
-VALUES	('RANDOM_EVENT_COMET_STRIKE',			'REALISM_SETTING_MEGADISASTERS',	50),
-		('RANDOM_EVENT_COMET_STRIKE',			'REALISM_SETTING_APOCALYPSE',		400),
-		('RANDOM_EVENT_COMET_STRIKE_TARGETED',	'REALISM_SETTING_MEGADISASTERS',	25),
-		('RANDOM_EVENT_COMET_STRIKE_TARGETED',	'REALISM_SETTING_APOCALYPSE',		200);
+        (RandomEventType,                        RealismSettingType,                    OccurrencesPerGame)
+VALUES  ('RANDOM_EVENT_COMET_STRIKE',            'REALISM_SETTING_MEGADISASTERS',    20),
+        ('RANDOM_EVENT_COMET_STRIKE',            'REALISM_SETTING_APOCALYPSE',       400),
+        ('RANDOM_EVENT_COMET_STRIKE_TARGETED',   'REALISM_SETTING_MEGADISASTERS',    10),
+        ('RANDOM_EVENT_COMET_STRIKE_TARGETED',   'REALISM_SETTING_APOCALYPSE',       200),
+        ('RANDOM_EVENT_METEOR_SHOWER',           'REALISM_SETTING_MEGADISASTERS',    30),
+        ('RANDOM_EVENT_METEOR_SHOWER',           'REALISM_SETTING_APOCALYPSE',       30);
