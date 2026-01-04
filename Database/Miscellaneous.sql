f705528b1e0c0aab33cbaa2c8a01fac1e3373e5e
Jake Kantzer <jkantzer@outlook.com>
2026-01-03 20:48:14 -0500

    Renamed file, science victory prereqs made faster

--- Database/Miscellaneous.sql
+++ Database/Miscellaneous.sql
@@ -5,3 +5,8 @@ UPDATE Units SET BaseMoves=BaseMoves+1 WHERE UnitType='UNIT_SETTLER';
 UPDATE Units SET BaseSightRange=BaseSightRange+1 WHERE UnitType='UNIT_SETTLER';
 --  Increase Movement of Embarked units
 UPDATE GlobalParameters SET Value=Value+1 WHERE Name='MOVEMENT_WHILE_EMBARKED_BASE';
+
+-- Reduce cost of Spaceport
+UPDATE Districts SET Cost=Cost*0.7 WHERE DistrictType='DISTRICT_SPACEPORT';
+-- Reduce cost of space projects
+UPDATE Projects SET Cost=Cost*0.7 WHERE SpaceRace=1
