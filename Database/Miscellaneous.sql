78e97e491cd71ca5f768b779aae5f0b9b81ceb29
Jake Kantzer <jkantzer@outlook.com>
2026-01-16 19:47:28 -0500

    bunch of irresponsible barbarian tweaks

--- Database/Miscellaneous.sql
+++ Database/Miscellaneous.sql
@@ -19,3 +19,19 @@ UPDATE Features SET DoubleAdjacentTerrainYield=1 WHERE FeatureType = 'FEATURE_CL
 
 -- Make Comet Lakes a little better
 UPDATE Features SET DoubleAdjacentTerrainYield=1 WHERE FeatureType = 'FEATURE_COMET_LAKE';
+
+-- Barbarian tweaks
+UPDATE GlobalParameters SET Value=Value/2 WHERE Name = 'BARBARIAN_CLANS_BRIBE_COST_PER_CITY';
+UPDATE GlobalParameters SET Value=Value*1.5 WHERE Name = 'BARBARIAN_CLANS_CIV_CONVERSION_INCREMENT_CHANCE';
+
+UPDATE GlobalParameters SET Value=Value-5 WHERE Name = 'BARBARIAN_BOLDNESS_PER_KILL';
+UPDATE GlobalParameters SET Value=Value+5 WHERE Name = 'BARBARIAN_BOLDNESS_PER_SCOUT_LOST';
+UPDATE GlobalParameters SET Value=Value+5 WHERE Name = 'BARBARIAN_BOLDNESS_PER_UNIT_LOST';
+UPDATE GlobalParameters SET Value=0 WHERE Name = 'BARBARIAN_BOLDNESS_PER_CAMP_ATTACK';
+
+UPDATE GlobalParameters SET Value=Value+1 WHERE Name = 'BARBARIAN_CAMP_MINIMUM_DISTANCE_CITY';
+UPDATE GlobalParameters SET Value=Value-1 WHERE Name = 'BARBARIAN_DEFENSE_ALL_UNITS';
+
+UPDATE BarbarianTribes SET RaidingBoldness=RaidingBoldness+5 WHERE Name='LOC_BARBARIAN_CLAN_TYPE_NAVAL';
+UPDATE BarbarianTribes SET CityAttackBoldness=CityAttackBoldness+5 WHERE Name='LOC_BARBARIAN_CLAN_TYPE_NAVAL';
+UPDATE BarbarianTribes SET DefenderTag = 'CLASS_RANGED' WHERE DefenderTag = 'CLASS_ANTI_CAVALRY';
